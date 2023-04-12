84979964426a3f4248060cb584ab0b3cd6497012
Brunk <125673909+NullBrunk@users.noreply.github.com>
2023-04-12 15:01:59 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,7 +46,7 @@
    */
   let navbarlinks = select('#navbar .scrollto', true)
   const navbarlinksActive = () => {
-    let position = window.scrollY + 300
+    let position = window.scrollY + 200
     navbarlinks.forEach(navbarlink => {
       if (!navbarlink.hash) return
       let section = select(navbarlink.hash)
