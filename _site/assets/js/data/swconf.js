8d7d6f008f455ac662b81df870ea6d185e3a2014
NullBrunk <anasi10@proton.me>
2024-12-26 16:11:02 +0100

    Update wpsh and arpspoofer post

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1735225698',resources: [
+    cacheName: 'chirpy-1735225854',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
