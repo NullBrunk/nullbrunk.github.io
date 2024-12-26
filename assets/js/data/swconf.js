5ed3e06d517ea76274c9debf8fa0ef62c4156e36
NullBrunk <anasi10@proton.me>
2024-12-26 16:40:43 +0100

    Update wpsh and arpspoofer images

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1735227229',resources: [
+    cacheName: 'chirpy-1735227633',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
