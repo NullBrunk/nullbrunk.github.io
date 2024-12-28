78e4245b64a517cc3d9e1fd0969a7ebbf54a0b63
NullBrunk <anasi10@proton.me>
2024-12-28 03:12:50 +0100

    added new walkthrough (cyberlens)

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1735351755',resources: [
+    cacheName: 'chirpy-1735351963',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
