389f84195edecd683146770cd49d55accacc82de
NullBrunk <anasi10@proton.me>
2024-12-29 21:38:32 +0100

    Add new writeup (blog)

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1735504580',resources: [
+    cacheName: 'chirpy-1735504704',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
