c09f643c17ef1ede4739421caf113280a38c40fd
NullBrunk <anasi10@proton.me>
2024-12-29 21:36:47 +0100

    Add new writeup (blof)

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1735504500',resources: [
+    cacheName: 'chirpy-1735504580',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
