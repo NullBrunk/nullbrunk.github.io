db0274d2d647975abb357d0083d7282e11b5dc3c
NullBrunk <anasi10@proton.me>
2024-12-29 21:41:04 +0100

    Add new writeup (blog)

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1735504704',resources: [
+    cacheName: 'chirpy-1735504853',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
