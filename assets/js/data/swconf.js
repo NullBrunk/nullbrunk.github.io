8a14265b317919a72ce3550da24cb6069f0def5a
NullBrunk <anasi10@proton.me>
2024-12-30 20:38:00 +0100

    Add new writeup (Attacktive Directory)

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1735504853',resources: [
+    cacheName: 'chirpy-1735587439',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
