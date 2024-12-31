c66704238f5a97d9981a5ddb484cc4ffef7a7ac2
NullBrunk <anasi10@proton.me>
2024-12-31 02:18:11 +0100

    Add new tool (xsserve)

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1735587439',resources: [
+    cacheName: 'chirpy-1735607779',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
