f8f085f85c85498ba8a46acdda3a4d8eb3b97411
NullBrunk <anasi10@proton.me>
2025-01-02 22:21:58 +0100

    Add new writeup (Whiterose)

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1735846260',resources: [
+    cacheName: 'chirpy-1735852910',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
