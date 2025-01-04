8ed5aaa16c4e6afb318cb72475ff37b32e979a87
NullBrunk <anasi10@proton.me>
2025-01-04 15:02:11 +0100

    Add opacity WU

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1735997859',resources: [
+    cacheName: 'chirpy-1735999325',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
