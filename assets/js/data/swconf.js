d390fda8dcdbd45983ee84c5415e0832e322b623
NullBrunk <anasi10@proton.me>
2025-01-13 03:01:23 +0100

    Add uoftctf2025

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1736003771',resources: [
+    cacheName: 'chirpy-1736733667',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
