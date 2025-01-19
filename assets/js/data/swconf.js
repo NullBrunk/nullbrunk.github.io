abd69fbd3720e038e18079f1799106f362644257
NullBrunk <anasi10@proton.me>
2025-01-20 00:55:52 +0100

    Add SrdnlenCTF

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1737330589',resources: [
+    cacheName: 'chirpy-1737330926',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
