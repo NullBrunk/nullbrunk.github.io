93df3002ea878884fe8900dc7cac6c2bd8a956db
NullBrunk <anasi10@proton.me>
2025-01-21 21:17:34 +0100

    Add pointer overflow

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1737442700',resources: [
+    cacheName: 'chirpy-1737490616',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
