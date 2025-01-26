0f79bc060f7da5a2b495df46aebcbed41e4164a0
NullBrunk <anasi10@proton.me>
2025-01-26 21:22:20 +0100

    Add hackdays

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1737922656',resources: [
+    cacheName: 'chirpy-1737922931',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
