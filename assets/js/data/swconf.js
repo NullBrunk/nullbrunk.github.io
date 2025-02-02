579eb558350b24bb42c6b3a949bc7f9691ce98e3
NullBrunk <anasi10@proton.me>
2025-02-02 18:10:47 +0100

    Modified sidebar

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738516072',resources: [
+    cacheName: 'chirpy-1738516234',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
