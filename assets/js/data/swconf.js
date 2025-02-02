881beee311ea2b27e0ecd55e6234c2bd875e4ec3
NullBrunk <anasi10@proton.me>
2025-02-02 16:59:36 +0100

    Add nullcon

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738511213',resources: [
+    cacheName: 'chirpy-1738511972',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
