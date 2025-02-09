729c2d009a9ac839f06a37a8de637f05da92fe98
NullBrunk <anasi10@proton.me>
2025-02-09 14:04:48 +0100

    Add pragyan

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738517545',resources: [
+    cacheName: 'chirpy-1739106267',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
