82f270ed22e4cb1d9530407896f869a02824147c
NullBrunk <anasi10@proton.me>
2025-08-04 11:38:27 +0200

    Building resume page

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1754300165',resources: [
+    cacheName: 'chirpy-1754300289',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
