f877d6acd3f9cc46cd66951311ac6d0f3ab88c05
NullBrunk <anasi10@proton.me>
2025-08-24 17:20:43 +0200

    Update

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1756038359',resources: [
+    cacheName: 'chirpy-1756048830',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
