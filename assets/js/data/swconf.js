c334eee7c9b21212917c60a80998f5b17297eb5e
NullBrunk <anasi10@proton.me>
2025-08-25 15:26:06 +0200

    Add badges

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1756048830',resources: [
+    cacheName: 'chirpy-1756128332',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
