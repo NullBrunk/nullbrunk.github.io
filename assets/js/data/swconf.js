639354e224fdb4a29365a32be6b2999c72bb9069
NullBrunk <anasi10@proton.me>
2025-09-07 21:41:24 +0200

    add imaginary ctf wu

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1757271044',resources: [
+    cacheName: 'chirpy-1757274071',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
