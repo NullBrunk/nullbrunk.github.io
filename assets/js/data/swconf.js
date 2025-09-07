41414ec3a0b5bd1ac319bc7011d952cea428bf2e
NullBrunk <anasi10@proton.me>
2025-09-07 21:12:40 +0200

    correct typos

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1756825030',resources: [
+    cacheName: 'chirpy-1757271044',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
