789a65c3c86918a4160ec118d6c950a7d15d6cf1
NullBrunk <anasi10@proton.me>
2026-01-25 12:51:01 +0100

    update pf

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1768979984',resources: [
+    cacheName: 'chirpy-1769341851',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
